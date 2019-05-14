07fea1b91e8440314e85228c316a2484d3af3180
sndnyangd <sndnyangd@gmail.com>
2019-05-14 10:33:08 -0400

    vat cifar10

--- torch_func/load_dataset.py
+++ torch_func/load_dataset.py
@@ -1,7 +1,4 @@
 import os
-import sys
-
-import pickle
 import numpy as np
 
 
@@ -31,13 +28,7 @@ def augmentation(images, random_crop=True, random_flip=True):
     return ret
 
 
-def _augmentation(images, trans=True, flip=True):
-    # shape of `image' [N, K, W, H]
-    assert images.ndim == 4
-    return augmentation(images, trans, flip)
-
-
-class Data2:
+class Data:
     def __init__(self, data, label):
         self.data = data
         self.label = label
@@ -58,102 +49,28 @@ class Data2:
         batch_data = self.data[index]
         batch_label = self.label[index]
         if aug_trans or aug_flip:
-            batch_data = _augmentation(batch_data, aug_trans, aug_flip)
+            assert batch_data.ndim == 4
+            # shape of `image' [N, K, W, H]
+            batch_data = augmentation(batch_data, aug_trans, aug_flip)
         return batch_data, batch_label
 
 
-class Data:
-    def __init__(self, data, label):
-        self.data = data
-        self.label = label
-        self.ind = np.arange(self.data.shape[0])
-        self.index = 0
-
-    @property
-    def N(self):
-        return len(self.data)
-
-    def get(self, n=None, aug_trans=False, aug_flip=False):
-        if n is None:
-            n = self.data.shape[0]
-        end = self.index + n
-        if end >= len(self.label):
-            index = self.ind[self.index:]
-            self.ind = np.random.permutation(self.data.shape[0])
-            remain = end % len(self.label)
-            if remain:
-                index = np.concatenate([index, self.ind[:remain]])
-        else:
-            index = self.ind[self.index:end]
-        batch_data = self.data[index]
-        batch_label = self.label[index]
-        self.index = end % self.data.shape[0]
-        if aug_trans or aug_flip:
-            batch_data = _augmentation(batch_data, aug_trans, aug_flip)
-
-        return batch_data, batch_label
-
-
-def load_mnist_dataset():
-    if sys.version_info.major == 3:
-        dataset = pickle.load(open('dataset/mnist.pkl', 'rb'), encoding="bytes")
+def load_dataset(dir_path, valid=False, dataset_seed=1):
+    if dataset_seed > 5:
+        # 1-5 -> [1, 5],  >5 -> [1, 5]
+        dataset_seed = dataset_seed % 5 + 1
+    if valid:
+        train_l = load_npz_as_dict(os.path.join(dir_path, 'seed' + str(dataset_seed), 'labeled_train_valid.npz'))
+        train_ul = load_npz_as_dict(os.path.join(dir_path, 'seed' + str(dataset_seed), 'unlabeled_train_valid.npz'))
+        test = load_npz_as_dict(os.path.join(dir_path, 'seed' + str(dataset_seed), 'test_valid.npz'))
     else:
-        dataset = pickle.load(open('dataset/mnist.pkl', 'rb'))
-    train_set_x = np.concatenate((dataset[0][0], dataset[1][0]), axis=0).astype("float32")
-    train_set_y = np.concatenate((dataset[0][1], dataset[1][1]), axis=0)
-    return (train_set_x, train_set_y), (dataset[2][0], dataset[2][1])
-
-
-def load_mnist_for_semi_sup(n_l=1000, n_v=1000):
-    dataset = load_mnist_dataset()
-
-    _train_set_x, _train_set_y = dataset[0]
-    test_set_x, test_set_y = dataset[1]
-
-    rand_ind = np.random.permutation(_train_set_x.shape[0])
-    _train_set_x = _train_set_x[rand_ind]
-    _train_set_y = _train_set_y[rand_ind]
-
-    s_c = int(n_l / 10.0)
-    train_set_x = np.zeros((n_l, 28 ** 2))
-    train_set_y = np.zeros(n_l)
-    for i in range(10):
-        ind = np.where(_train_set_y == i)[0]
-        train_set_x[i * s_c:(i + 1) * s_c, :] = _train_set_x[ind[0:s_c], :]
-        train_set_y[i * s_c:(i + 1) * s_c] = _train_set_y[ind[0:s_c]]
-        _train_set_x = np.delete(_train_set_x, ind[0:s_c], 0)
-        _train_set_y = np.delete(_train_set_y, ind[0:s_c])
-
-    l_rand_ind = np.random.permutation(train_set_x.shape[0])
-    train_set_x = train_set_x[l_rand_ind].astype("float32")
-    train_set_y = train_set_y[l_rand_ind]
-    train_set_ul_x = np.concatenate((train_set_x, _train_set_x[n_v:]), axis=0)
-    train_set_ul_x = train_set_ul_x[np.random.permutation(train_set_ul_x.shape[0])]
-    train_set_ul_y = np.concatenate((train_set_y, _train_set_y[n_v:]), axis=0)
-
-    return (train_set_x, train_set_y), (train_set_ul_x, train_set_ul_y), (test_set_x, test_set_y)
-
-
-def load_dataset(dirpath, valid=False, dataset_seed=1, size=0):
-    if 'mnist' in dirpath:
-        train_l, train_ul, test_set = load_mnist_for_semi_sup(n_l=size)
-        train_l = {'images': train_l[0], 'labels': train_l[1]}
-        train_ul = {'images': train_ul[0], 'labels': train_ul[1]}
-        test = {'images': test_set[0], 'labels': test_set[1]}
-    else:
-        if valid:
-            train_l = load_npz_as_dict(os.path.join(dirpath, 'seed' + str(dataset_seed), 'labeled_train_valid.npz'))
-            train_ul = load_npz_as_dict(os.path.join(dirpath, 'seed' + str(dataset_seed), 'unlabeled_train_valid.npz'))
-            test = load_npz_as_dict(os.path.join(dirpath, 'seed' + str(dataset_seed), 'test_valid.npz'))
-        else:
-            train_l = load_npz_as_dict(os.path.join(dirpath, 'seed' + str(dataset_seed), 'labeled_train.npz'))
-            train_ul = load_npz_as_dict(os.path.join(dirpath, 'seed' + str(dataset_seed), 'unlabeled_train.npz'))
-            test = load_npz_as_dict(os.path.join(dirpath, 'seed' + str(dataset_seed), 'test.npz'))
-        train_l['images'] = train_l['images'].reshape(train_l['images'].shape[0], 3, 32, 32).astype(np.float32)
-        train_ul['images'] = train_ul['images'].reshape(train_ul['images'].shape[0], 3, 32, 32).astype(np.float32)
-        test['images'] = test['images'].reshape(test['images'].shape[0], 3, 32, 32).astype(np.float32)
-    # mnist 100 use data
-    # larger data set should use Data2 as original one
-    return Data(train_l['images'], train_l['labels'].astype(np.int32)), \
-           Data(train_ul['images'], train_ul['labels'].astype(np.int32)), \
-           Data(test['images'], test['labels'].astype(np.int32))
+        train_l = load_npz_as_dict(os.path.join(dir_path, 'seed' + str(dataset_seed), 'labeled_train.npz'))
+        train_ul = load_npz_as_dict(os.path.join(dir_path, 'seed' + str(dataset_seed), 'unlabeled_train.npz'))
+        test = load_npz_as_dict(os.path.join(dir_path, 'seed' + str(dataset_seed), 'test.npz'))
+    train_l['images'] = train_l['images'].reshape(train_l['images'].shape[0], 3, 32, 32).astype(np.float32)
+    train_ul['images'] = train_ul['images'].reshape(train_ul['images'].shape[0], 3, 32, 32).astype(np.float32)
+    test['images'] = test['images'].reshape(test['images'].shape[0], 3, 32, 32).astype(np.float32)
+    train_l['labels'] = train_l['labels'].astype(np.int32)
+    train_ul['labels'] = train_ul['labels'].astype(np.int32)
+    test['labels'] = test['labels'].astype(np.int32)
+    return Data(train_l['images'], train_l['labels']), Data(train_ul['images'], train_ul['labels']), Data(test['images'], test['labels'])
