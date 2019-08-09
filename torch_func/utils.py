59316dd97919892a11e155410c65c3a23f232fd1
sndnyang <xiulong.yancey.yang@gmail.com>
2019-08-09 15:26:42 -0400

    torch

--- torch_func/utils.py
+++ torch_func/utils.py
@@ -2,10 +2,10 @@ import os
 import sys
 import random
 
-import torch
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
 import torch.nn.functional as nfunc
-from torch.nn.parameter import Parameter
 
 
 def call_bn(bn, x, update_batch_stats=True):
@@ -49,7 +49,7 @@ def weights_init_uniform(m):
         s = 1.0 * np.sqrt(6.0 / fan_in)
         transpose = np.random.uniform(-s, s, m.weight.data.shape).astype("float32")
         tensor = torch.from_numpy(transpose)
-        m.weight = Parameter(tensor, requires_grad=True)
+        m.weight = torch.nn.parameter.Parameter(tensor, requires_grad=True)
         if m.bias is not None:
             m.bias.data.zero_()
 
@@ -76,36 +76,91 @@ def weights_init_normal(m):
         transpose = np.random.normal(0, s, m.weight.data.shape[::-1]).astype("float32").T
         tensor = torch.from_numpy(transpose)
         # print(shape, transpose.sum())
-        m.weight = Parameter(tensor, requires_grad=True)
+        m.weight = torch.nn.parameter.Parameter(tensor, requires_grad=True)
         if m.bias is not None:
             m.bias.data.zero_()
 
 
-def adjust_learning_rate(optimizer, epoch, args):
+def adjust_learning_rate(optimizer, epoch, args, group=1):
     """Sets the learning rate from start_epoch linearly to zero at the end"""
     if epoch < args.epoch_decay_start:
         return args.lr
     lr = float(args.num_epochs - epoch) / (args.num_epochs - args.epoch_decay_start) * args.lr
-    for param_group in optimizer.param_groups:
+    if args.dataset == "cifar10":
+        lr *= 0.2
+    for i, param_group in enumerate(optimizer.param_groups):
         param_group['lr'] = lr
+        if group > 1 and i > 0:
+            continue
         param_group['betas'] = (0.5, 0.999)
     return lr
 
 
 def load_checkpoint_by_marker(args, exp_marker):
-    base_dir = os.path.join(os.environ['HOME'], 'project/runs')
+    base_dir = os.path.join(os.environ['HOME'], 'project/runs') if not args.log_dir else args.log_dir
     dir_path = os.path.join(base_dir, exp_marker)
     c = 0
     file_name = ""
+    # example 060708/80 -> dir path contains 060708 and model_80.pth
+    parts = args.resume.split("@")
     for p in os.listdir(dir_path):
-        if args.resume in p:
+        if parts[0] in p:
             c += 1
             if c == 2:
                 print("can't resume, find 2")
                 sys.exit(-1)
-            file_name = os.path.join(dir_path, p, "model.pth")
+            file_name = os.path.join(dir_path, p, "model.pth" if len(parts) == 1 else "model_%s.pth" % parts[1])
     if file_name == "":
         print("can't resume, find 0")
         sys.exit(-1)
     checkpoint = torch.load(file_name)
     return checkpoint
+
+
+def l2_normalize(d):
+    t = d.clone()    # remove from the computing graph
+    # norm = torch.sqrt(torch.sum(t.view(d.shape[0], -1) ** 2, dim=1))
+    norm = torch.norm(t.view(d.shape[0], -1), p=2, dim=1)
+    if len(t.shape) < 1:
+        raise NotImplementedError
+    # shape [128] -> [128, 1, 1, 1] to match the images' shape [128, c, h, w]
+    target_shape = [1] * len(t.shape)
+    target_shape[0] = -1
+    normed_d = t / (norm.view(target_shape) + 1e-10)
+    return normed_d
+
+
+def entropy(logits):
+    p = nfunc.softmax(logits, dim=1)
+    return torch.mean(torch.sum(p * nfunc.log_softmax(logits, dim=1), dim=1))
+
+
+def show_image(image, args):
+    if isinstance(image, torch.Tensor):
+        if image.device.type == "cuda":
+            image = image.cpu().detach()
+        image = image.numpy()
+
+    if args.dataset == "mnist":
+        image = image[0]
+    elif args.dataset == "svhn":
+        image = image.transpose((1, 2, 0))
+        if args.data_dir != "data1.0":
+            image += 0.5
+    else:
+        image = image.transpose((1, 2, 0))
+
+    c_bar = plt.imshow(image)
+    plt.colorbar(c_bar)
+    plt.show()
+    plt.close()
+
+
+def stat_of_model(model):
+    l = 0
+    for p in model.parameters():
+        print("layer weight", p.sum())
+        print("layer grad ", p.grad.sum())
+        l += 1
+        if l > 3:
+            break
