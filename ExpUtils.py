5cedae81c0e16a29b946cf48e2c412b6cf0b22b4
sndnyang <xiulong.yancey.yang@gmail.com>
2019-08-09 15:27:35 -0400

    Update ExpUtils.py

--- ExpUtils.py
+++ ExpUtils.py
@@ -1,243 +1,110 @@
 import os
-import re
 import sys
 import json
 import time
-import errno
 import shutil
-import random
+import signal
 import logging
-import argparse
+from functools import partial
 
+import torch
 import numpy as np
-from tensorboardX import SummaryWriter
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(filename)s[line:%(lineno)d]: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(filename)s[%(lineno)d]: %(message)s", datefmt="%m-%d %H:%M:%S")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 wlog = logger.info
-exp_seed = random.randrange(sys.maxsize) % 10000
 
 
-def set_framework_seed(seed):
-    random.seed(seed)
-    np.random.seed(seed)
+def auto_select_gpu():
     try:
-        import torch
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
+        import GPUtil
     except ImportError:
-        pass
-    try:
-        import cupy as cp
-        cp.random.seed(seed)
-    except ImportError:
-        pass
+        wlog("please install GPUtil for automatically selecting GPU")
+        return ""
+    if len(GPUtil.getGPUs()) == 0:
+        return ""
+    id_list = GPUtil.getAvailable(order="load", maxLoad=0.7, maxMemory=0.7, limit=8)
+    if len(id_list) == 0:
+        print("GPU memory is not enough for predicted usage")
+        raise NotImplementedError
+    return str(id_list[0])
 
 
-def make_sure_path_exists(path):
-    try:
-        os.makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+def set_file_logger(exp_logger, args):
+    # use tensorboard + this function to substitute ExpSaver
+    args_dict = vars(args)
+    dir_path = args.dir_path
+    if not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
+    with open(os.path.join(dir_path, "para.json"), "w") as fp:
+        json.dump(args_dict, fp, indent=4, sort_keys=True)
+    logfile = os.path.join(dir_path, "exp.log")
+    fh = logging.FileHandler(logfile, mode='w')
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d]: %(message)s")
+    fh.setFormatter(formatter)
+    exp_logger.addHandler(fh)
+    if os.name != 'nt':
+        signal.signal(signal.SIGQUIT, partial(rename_quit_handler, args))
+        signal.signal(signal.SIGTERM, partial(delete_quit_handler, args))
 
 
-def get_cmp_para(params, keys):
-    suffix = ""
-    for e in keys:
-        assert e in params
-        suffix += "_%s=%s" % (e, str(params[e] if not isinstance(params, float) else "%g" % params[e]))
-    return suffix
+def list_args(args):
+    for e in sorted(vars(args).items()):
+        print("args.%s = %s" % (e[0], e[1] if not isinstance(e[1], str) else '"%s"' % e[1]))
 
 
-class ExpSaver:
-
-    def __init__(self, task_solver, args, dir_keys=None, file_keys=None):
-        if dir_keys is None:
-            dir_keys = []
-        if file_keys is None:
-            file_keys = []
-        assert task_solver is not None
-        self.run_time = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-        args.task_solver = task_solver
-        if not isinstance(args, dict):
-            # sometimes, the code uses docopt library.
-            self.args = args
+def form_dir_path(task, args):
+    args_dict = vars(args)
+    run_time = time.strftime('%m%d%H%M%S', time.localtime(time.time()))
+    log_arg_list = []
+    for e in args.log_arg.split("-"):
+        v = args_dict.get(e, None)
+        if v is None:
+            log_arg_list.append(str(e))
+        elif isinstance(v, str):
+            log_arg_list.append(str(v))
         else:
-            self.args = argparse.Namespace()
-            self.args.__dict__.update(args)
-        self.task, self.trainer = task_solver.split("-")[:2]
-        self.exp_marker = get_cmp_para(args.__dict__, file_keys)
-        self.log_dir = self.set_log_path(self.trainer, dir_keys)
-        self.exp_seed = exp_seed
-        self.save_params()
-        self.keys = file_keys
-        self.writer = None
-        self.board_path = ""
-
-    def update_args(self, args):
-        if not isinstance(args, dict):
-            self.args = args
-        self.exp_marker = get_cmp_para(self.args.__dict__, self.keys)
-
-    def set_log_path(self, trainer, tuned=None):
-        args = self.args.__dict__
-        base = os.path.join(os.environ["HOME"], "project", "results", self.task, args.get("dataset") if "dataset" in args else "toy")
-
-        marker = trainer
-        if tuned:
-            marker += "-" + "-".join("%s=%s" % (e, str(self.args.__dict__.get(e))) for e in tuned)
-            log_dir = os.path.join(base, "%s-%s_running" % (marker, self.run_time))
-        else:
-            log_dir = os.path.join(base, "%s-%s_running" % (marker, self.run_time))
-        log_name = log_dir + '/logs_%s_%s.log' % (self.run_time, exp_seed)
-
-        os.makedirs(log_dir)
-        os.makedirs(os.path.join(log_dir, "npy"))
-        os.makedirs(os.path.join(log_dir, "txt"))
-        logfile = log_name
-        fh = logging.FileHandler(logfile, mode='w')
-        fh.setLevel(logging.INFO)
-        formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d]: %(message)s")
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-        return log_dir
-
-    def delete_dir(self):
-        shutil.rmtree(self.log_dir)
-        if self.board_path:
-            shutil.rmtree(self.board_path)
-
-    def finish_exp(self, keep_args=None):
-        if keep_args is None:
-            keep_args = {}
-        if "epochs" in self.args and (self.args.epochs < 50 or (self.args.dataset in ["cifar10", "cifar100"] and self.args.epochs < 20)):
-            shutil.rmtree(self.log_dir)
-            return
-        if "iterations" in self.args and self.args.iterations < 100:
-            shutil.rmtree(self.log_dir)
-            return
-        args_dict = vars(self.args)
-        for k in keep_args:
-            if k in self.args and args_dict[k] < keep_args[k]:
-                self.delete_dir()
-                return
-        wlog("args_list in this experiment %s", '\n'.join(str(e) for e in sorted(args_dict.items())))
-        os.rename(self.log_dir, self.log_dir[:-8])
-
-    def save_params(self):
-        params = {}
-        for e in self.args.__dict__:
-            val = self.args.__dict__[e]
-            if not isinstance(val, (int, float, bool, tuple, str, list, dict)):
-                continue
-            params[e] = val
-
-        if self.log_dir:
-            with open(os.path.join(self.log_dir, "params.json"), "w") as fp:
-                json.dump(params, fp, indent=4, sort_keys=True)
-
-        return params
-
-    def save_hist(self, hist, metric="acc"):
-        for e in ["train", "val", "test"]:
-            if e not in hist:
-                continue
-            loss_array = np.array([e[1] for e in hist.get(e)])
-            self.save_npy(loss_array, name="%s_loss_" % e)
-            array = np.array([e[0] for e in hist.get(e)])
-            self.save_npy(array, name="%s_%s_" % (e, metric))
-
-    def save_npy(self, array, name=""):
-        # make sure the log file and experimental results will save to different directories
-        marker = (self.run_time, exp_seed)
-
-        # default way is to save as .npy file, however I like save as a text file.
-        # so save both.
-        if self.args.exp == "avg":
-            file_name = os.path.join(self.log_dir, "%s%s.npy" % (name, self.exp_marker))
-        else:
-            file_name = os.path.join(self.log_dir, "npy", "%s%s.npy" % (name, self.exp_marker))
-        np.save(file_name, array)
-        file_name = os.path.join(self.log_dir, "txt", "%s%s.npy.txt" % (name, self.exp_marker))
-        np.savetxt(file_name, array[np.newaxis], fmt='%g', delimiter=',')
-        return marker
-
-    def save_figure(self, figure, name=""):
-        figure.savefig(os.path.join(self.log_dir, "exp_results_%s%s.png" % (self.exp_marker, name)))
-
-    def init_writer(self, para):
-        if para is None:
-            para = []
-        para_str = '-'.join("%s=%s" % (e, str(self.args.__dict__[e])) for e in para)
-        dir_marker = os.path.join(self.task, self.args.dataset, "%s_%s_%s_%s" % (self.trainer, para_str, str(self.args.exp), self.run_time))
-        print(self.task, self.trainer)
-        dir_path = os.path.join(os.environ['HOME'], 'project/runs', dir_marker)
-        self.board_path = dir_path
-        self.writer = SummaryWriter(log_dir=dir_path)
+            log_arg_list.append("%s=%s" % (e, str(v)))
+    args.exp_marker = exp_marker = "-".join(log_arg_list)
+    exp_marker = "%s/%s/%s@%s@%d" % (task, args.dataset, run_time, exp_marker, os.getpid())
+    base_dir = os.path.join(os.environ['HOME'], 'project/runs') if not args.log_dir else args.log_dir
+    dir_path = os.path.join(base_dir, exp_marker)
+    return dir_path
 
 
-class ExpLoader:
-    """
-    TODO:
-        useless
-    """
-    def __init__(self, path):
-        self.path = path
-        self.args = {}
-        self.load(path)
+def summary(data):
+    assert isinstance(data, np.ndarray) or isinstance(data, torch.Tensor)
+    wlog("shape: %s, num of points: %d, pixels: %d" % (str(data.shape), data.shape[0], np.prod(data.shape[1:])))
+    wlog("max: %g, min %g" % (data.max(), data.min()))
+    wlog("mean: %g" % data.mean())
+    wlog("mean of abs: %g" % np.abs(data).mean())
+    wlog("mean of square sum: %g" % (data ** 2).mean())
 
-    def load(self, path):
-        args = {}
-        data_set, size, method = path.split("/")[5:8]
-        args["data-set"] = data_set
-        args["data-size"] = size
-        args["method"] = method
-        args["metrics"] = set([])
 
-        for f in os.listdir(path):
-            if not f.endswith(".npy"):
-                continue
-            parts = f[:-4].split("_")
-            for e in parts:
-                if "=" in e:
-                    continue
-                args["metrics"].add(e)
-            for e in parts:
-                if "=" not in e:
-                    continue
-                key, value = e.split("=")
-                if key not in args:
-                    args[key] = set([])
-                args[key].add(value)
-        self.args = args
-        return args
+def remove_outliers(x, outlier_constant=1.5):
+    a = np.array(x)
+    upper_quartile = np.percentile(a, 75)
+    lower_quartile = np.percentile(a, 25)
+    iqr = (upper_quartile - lower_quartile) * outlier_constant
+    quartile_set = (lower_quartile - iqr, upper_quartile + iqr)
 
-    def avg_experiments(self):
-        for f in os.listdir(self.path + "/npy"):
-            if not f.endswith(".npy"):
-                continue
-            if "exp=1_" not in f and "exp=0_" not in f:
-                continue
-            f = re.sub("exp=[01]_", "exp=avg_", f)
-            if os.path.isfile(os.path.join(self.path, f)):
-                print("%s exists" % f)
-                continue
-            acc_list = []
-            for e in self.args.get("exp", []):
-                if e == "avg":
-                    continue
-                exp_f = f.replace("exp=avg_", "exp=%s_" % e)
-                exp_f = os.path.join(self.path, "npy", exp_f)
-                if not os.path.isfile(exp_f):
-                    continue
-                acc_list.append(np.load(exp_f))
-            if len(acc_list) == 0:
-                continue
-            array = np.array(acc_list)
-            array = np.mean(array, axis=0)
+    result = a[np.where((a >= quartile_set[0]) & (a <= quartile_set[1]))]
 
-            np.save(os.path.join(self.path, f), array)
-            np.savetxt(os.path.join(self.path, "txt", f), array[np.newaxis], fmt='%g', delimiter=',')
+    return result
+
+
+def vis_step(writer, step, dicts):
+    for k in dicts:
+        writer.add_scalar(k, dicts[k], step)
+
+
+def delete_quit_handler(g_var, signal, frame):
+    shutil.rmtree(g_var.dir_path)
+    sys.exit(0)
+
+
+def rename_quit_handler(g_var, signal, frame):
+    os.rename(g_var.dir_path, g_var.dir_path + "_stop")
+    sys.exit(0)
